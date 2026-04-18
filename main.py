b5bddeab23f72784c33d7a0c1f6ddda4f9b45d25
ItsBirykolka <birykolka_official@mail.ru>
2026-04-18 22:19:38 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import time
+import random
+import requests
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+messages = [
+    "Лескин сын хуйни",
+    "Лескин сын шлюхи",
+    "Лескин сын пидора",
+    "Лескин сын бляди",
+]
+
+def send(text):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+
+while True:
+    msg = random.choice(messages)
+    send(msg)
+    print("Отправлено:", msg)
+    time.sleep(5 * 60 * 60)
